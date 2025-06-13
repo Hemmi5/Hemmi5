91030242851d38274507f18185f975ee8e01518f
Hemmi5 <hemmeraj@mail.uc.edu>
2025-06-13 15:31:55 -0400

    index, waph and adding script update

---
+++ script.js
@@ -0,0 +1,30 @@
+function checkVisit() {
+  const welcome = document.getElementById("welcome-message");
+  const lastVisit = getCookie("lastVisit");
+  const now = new Date().toLocaleString();
+
+  if (!lastVisit) {
+    welcome.innerHTML = "Welcome to my homepage for the first time!";
+  } else {
+    welcome.innerHTML = "Welcome back! Your last visit was " + lastVisit;
+  }
+
+  setCookie("lastVisit", now, 365);
+}
+
+function setCookie(name, value, days) {
+  const d = new Date();
+  d.setTime(d.getTime() + (days*24*60*60*1000));
+  document.cookie = name + "=" + value + ";expires=" + d.toUTCString() + ";path=/";
+}
+
+function getCookie(name) {
+  const cname = name + "=";
+  const decoded = decodeURIComponent(document.cookie);
+  const cookies = decoded.split(';');
+  for (let c of cookies) {
+    while (c.charAt(0) === ' ') c = c.substring(1);
+    if (c.indexOf(cname) === 0) return c.substring(cname.length, c.length);
+  }
+  return "";
+}
